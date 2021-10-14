ab17ec800f890ac42581dbba1768b6dffc692d45
Kethly <Kethlyfast@gmail.com>
2021-10-14 01:25:05 -0400

    final commit

--- hello.sh
+++ hello.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "here I do it"
 echo "Start of convo b/w Thien and Ultimate Godly Overlord who is aslo a genius"
-echo "The genius spelled also wrong.  If only Carter wasn't lazy and also here with us we could've gone 3 ways."
+echo "The genius spelled also wrong.  If only Carter wasn't lazy and also here with us we could've gone 3 ways"
 echo "Now we gotta slander him cause he ain't here >:), wat time u gonna sleep/wake up? Also 4 hrs might have been a mistake..."
+echo "my wake up time is 10am basically tomorrow. Got class at 10:40, I haven't been doing work at all today tho lmao"
 
