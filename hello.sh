1a8b0376c228bcc559f2363840b19c7ec573ed28
Kethly <Kethlyfast@gmail.com>
2021-10-14 01:05:43 -0400

    second commit after dhruvs

--- hello.sh
+++ hello.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "here I do it"
 echo "Start of convo b/w Thien and Ultimate Godly Overlord who is aslo a genius"
+echo "The genius spelled also wrong.  If only Carter wasn't lazy and also here with us we could've gone 3 ways."
