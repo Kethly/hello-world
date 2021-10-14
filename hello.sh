f95e57badb9dc61ee31ccce6639c366d738a3c5b
Dhruv Joshi <dhruvj25@gmail.com>
2021-10-14 01:20:53 -0400

    "3rd commit"

--- hello.sh
+++ hello.sh
@@ -2,3 +2,5 @@
 echo "here I do it"
 echo "Start of convo b/w Thien and Ultimate Godly Overlord who is aslo a genius"
 echo "The genius spelled also wrong.  If only Carter wasn't lazy and also here with us we could've gone 3 ways."
+echo "Now we gotta slander him cause he ain't here >:), wat time u gonna sleep/wake up? Also 4 hrs might have been a mistake..."
+
